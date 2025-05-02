83578c5966a397afd5afdc0cf829762f97127f9e
GuSantiagoNog <gsantiagonogueira@gmail.com>
2025-05-02 14:28:13 -0300

    primeiro commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+const pedido = {};
+let total = 0;
+
+function adicionarItem(nome, preco) {
+  if (!pedido[nome]) {
+    pedido[nome] = { quantidade: 0, preco: preco };
+  }
+  pedido[nome].quantidade++;
+  total += preco;
+  atualizarTela();
+}
+
+function atualizarTela() {
+  const lista = document.getElementById('lista');
+  lista.innerHTML = '';
+  for (const item in pedido) {
+    if (pedido[item].quantidade > 0) {
+      const subtotal = pedido[item].quantidade * pedido[item].preco;
+      lista.innerHTML += `<p>${pedido[item].quantidade} ${item} - R$${subtotal}</p>`;
+    }
+  }
+  document.getElementById('total').innerText = total;
+}
+
+function limpar() {
+  for (const item in pedido) {
+    pedido[item].quantidade = 0;
+  }
+  total = 0;
+  atualizarTela();
+}
